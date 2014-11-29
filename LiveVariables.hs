41037f391d02e5f8d929e3cef311d2e4a7eb49b9
Danil <danil.v.annenkov@gmail.com>
2014-11-29 17:32:47 +0100

    Added live variables equations solving

--- LiveVariables.hs
+++ LiveVariables.hs
@@ -1,13 +1,13 @@
-module AvailableExpr where
+module LiveVariables where
 
 import qualified Data.Set as S
 import Data.List
 import Debug.Trace
 
 data Expr = 
-          AEin Int
-          | AEout Int
-          | Set (S.Set (String, Aexpr))
+          LVin Int
+          | LVout Int
+          | Set (S.Set String)
           | Diff Expr Expr
           | U Expr Expr
           | Join Expr Expr 
@@ -18,8 +18,8 @@ _s xs = Set $ S.fromList xs
 
 
 instance Show Expr where
-  show (AEin i) = "AEin(" ++ show i ++ ")"
-  show (AEout i) = "AEout(" ++ show i ++ ")"
+  show (LVin i) = "LVin(" ++ show i ++ ")"
+  show (LVout i) = "LVout(" ++ show i ++ ")"
   show (Set s) = "{" ++ intercalate "," (map show $ S.toList s) ++ "}"
   show (Diff e1 e2) = show e1 ++ " \\ " ++ show e2
   show (U e1 e2) = show e1 ++ " U " ++ show e2
@@ -29,8 +29,8 @@ instance Show Expr where
 ppExpr :: [Expr] -> String
 ppExpr = intercalate "\n" . map show
 
-ppResult (rdins, rdouts) = "AEin: \n" ++ ppExpr rdins
-                           ++ "\n\nAEout: \n" ++ ppExpr rdouts ++ "\n"
+ppResult (rdins, rdouts) = "LVin: \n" ++ ppExpr rdins
+                           ++ "\n\nLVout: \n" ++ ppExpr rdouts ++ "\n"
 
 e1 .\ e2 = Diff e1 e2
 
@@ -53,43 +53,46 @@ instance Show Aexpr where
   show (Minus e1 e2) = show e1 ++ "-" ++ show e2
   show (Lit i) = show i 
   show (Var v) = show v
+
+v name = Var name
   
 instance Num Aexpr where
   (*) = Mul
   (+) = Add
   (-) = Minus
 
-vars = ["x", "y", "a", "b"] -- "Var" set
-aExpr = [Add (Var "a") (Var "b"),
-         Mul (Var "a") (Var "b"),
-         Add (Var "a") (Lit 1)]
-
-a `inExpr` (Mul e1 e2)  = (a `inExpr` e1) || (a `inExpr` e2)
-a `inExpr` (Add e1 e2)  = (a `inExpr` e1) || (a `inExpr` e2)
-a `inExpr` (Lit _)  = False
-a `inExpr` (Var x) | a == x  = True
-                   | otherwise = False
+vars = ["x", "y", "z"] -- "Var" set
 
 
-kill a =  _s [(v,e) | v <- vars,  e <- aExpr, a `inExpr` e] `U` _s [(a, e) | e <- aExpr]
+varsOf (Mul e1 e2)  = varsOf e1 ++ varsOf e2
+varsOf (Add e1 e2)  = varsOf e1 ++ varsOf e2
+varsOf (Lit _)  = []
+varsOf (Var x) = [x]
+
+
+kill a =  _s a
 
 -- use _s to make a set from list of pairs
 
-eqs = [ AEin  1 .=. _s [] ,
-        AEin  2 .=. AEout 1,
-        AEin  3 .=. AEout 2 `Join` (AEout 5),
-        AEin  4 .=. AEout 3,
-        AEin  5 .=. AEout 4,
+eqs = [ LVin  1 .=. LVout 1 .\ kill ["x"],
+        LVin  2 .=. LVout 2 .\ kill ["y"],
+        LVin  3 .=. LVout 3 .\ kill ["x"],
+        LVin  4 .=. LVout 4 `U` _s ["x", "y"],
+        LVin  5 .=. LVout 5 .\ kill ["z"] `U` _s ["y"],
+        LVin  6 .=. LVout 6 .\ kill ["z"] `U` _s ["y"],
+        LVin  7 .=. LVout 7 .\ kill ["x"] `U` _s ["z"],
         
-        AEout 1 .=. AEin  1 .\ kill "x" `U` _s [("x", Var "a" + Var "b")],
-        AEout 2 .=. AEin  2 .\ kill "y" `U` _s [("y", Var "a" * Var "b")],
-        AEout 3 .=. AEin  3,
-        AEout 4 .=. AEin  4 .\ kill "a",
-        AEout 5 .=. AEin  5 .\ kill "x" `U` _s [("x", Var "a" + Var "b")]
+        LVout 1 .=. LVin 2,
+        LVout 2 .=. LVin 3,
+        LVout 3 .=. LVin 4,
+        LVout 4 .=. LVin 5 `U` LVin 6,
+        LVout 5 .=. LVin 7,
+        LVout 6 .=. LVin 7,
+        LVout 7 .=. _s []
       ]
 
 -- TODO add ordering!!!
-getDefs eqs = ([e1 | Eqq (AEin _) e1 <- eqs], [e2 | Eqq (AEout _) e2 <- eqs])
+getDefs eqs = ([e1 | Eqq (LVin _) e1 <- eqs], [e2 | Eqq (LVout _) e2 <- eqs])
 
 substRD v rd s@(Set _) = s
 substRD v rd (Diff e1 e2) = Diff (substRD v rd e1) (substRD v rd e2)
@@ -100,10 +103,10 @@ substRD v rd old
               | otherwise = old
 
 substAllins [] _ outs = outs
-substAllins (v:vs) i outs = substAllins vs (i+1) (map (substRD v (AEin i)) outs)
+substAllins (v:vs) i outs = substAllins vs (i+1) (map (substRD v (LVin i)) outs)
 
 substAllouts [] _ ins = ins
-substAllouts (v:vs) i ins = substAllouts vs (i+1) (map (substRD v (AEout i)) ins)
+substAllouts (v:vs) i ins = substAllouts vs (i+1) (map (substRD v (LVout i)) ins)
 
 eval (U s1 s2) = S.union (eval s1) (eval s2)
 eval (Join s1 s2) = S.intersection (eval s1) (eval s2)
@@ -129,8 +132,7 @@ solve eqs = fixPoint init defs ([],[])
           where
             defs = getDefs eqs
             -- actually, length (fst defs) == length (snd defs) :)
-            init = (replicate (length $ fst defs) (_s [(v, e) | v <- vars, e <- aExpr]), 
-                    replicate (length $ snd defs) (_s [(v, e) | v <- vars, e <- aExpr]))
-
+            init = (map Set $ replicate (length $ fst defs) S.empty, 
+                    map Set $ replicate (length $ snd defs) S.empty)
 -- usage
 main = putStr $ ppResult $ solve eqs
